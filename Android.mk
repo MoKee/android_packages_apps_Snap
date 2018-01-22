7cf479067a94fc4feb2a356c36979d644b074549
martincz <martincz@mokeedev.com>
2018-01-23 06:25:00 +0800

    Snap: rebrand to org.mokee.snap
    Change-Id: I448124a8d858b6d61ed33afde69332ceb87a8974

--- Android.mk
+++ Android.mk
@@ -31,12 +31,12 @@ LOCAL_STATIC_JAVA_AAR_LIBRARIES += \
     qreader-core \
     qreader-zxing
 
-LOCAL_JAVA_LIBRARIES := org.lineageos.platform.internal
+LOCAL_JAVA_LIBRARIES := org.mokee.platform.internal
 
 LOCAL_PACKAGE_NAME := Snap
 LOCAL_PRIVILEGED_MODULE := true
 
-LOCAL_AAPT_FLAGS += --rename-manifest-package org.lineageos.snap
+LOCAL_AAPT_FLAGS += --rename-manifest-package org.mokee.snap
 
 #LOCAL_SDK_VERSION := current
 LOCAL_RENDERSCRIPT_TARGET_API := 23
