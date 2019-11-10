0403ed71c0366c39a18b4fcd58a9736a5a75226c
Martincz Gao <martincz@mokeedev.com>
2019-11-10 23:25:00 +0800

    Snap: mokee-ify
    Change-Id: Ic72e67c4e72e0f1a33663d8a0c310fdb63eb8376

--- quickReader/src/org/mokee/quickreader/ScannerActivity.java
+++ quickReader/src/org/mokee/quickreader/ScannerActivity.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.lineageos.quickreader;
+package org.mokee.quickreader;
 
 import android.Manifest;
 import android.app.Activity;
